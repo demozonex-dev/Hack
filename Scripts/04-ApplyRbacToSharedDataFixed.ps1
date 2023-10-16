7dbeff1a42e9c9ab75e53032ac80f11d1e220615
Eric Vernié <eric.vernie@hotmail.Fr>
2023-10-16 17:51:19 +0200

    add/remove rbac on shared data

---
+++ Scripts/04-ApplyRbacToSharedDataFixed.ps1
@@ -0,0 +1,33 @@
+## 
+
+
+$AADGroups=az ad group list --query "[?contains(displayName, 'HKT')].{id: id, displayName: displayName}" | ConvertFrom-Json
+
+$id=az group show -g "rg-SharedData" --query id
+$ResourceGroupName="rg-SharedData"
+
+Foreach ($AADGroup in $AADGroups)
+{
+           
+            if ($AADGroup.displayName -eq "HKT-DATADESK")
+            {
+                continue
+            }
+            write-host $AADGroup.displayName
+            # write-host "Applying RBAC to  the resource group  '$ResourceGroupName'"
+            $AadGroupId=$AADGroup.id
+            
+            # az role assignment delete --resource-group $ResourceGroupName --assignee $AadGroupId --role "Storage Blob Data Contributor" --scope $id  --query roleDefinitionId
+            # az role assignment delete --resource-group $ResourceGroupName --assignee $AadGroupId --role "Contributor" --scope $id  --query roleDefinitionId
+
+            # az role assignment  create --resource-group $ResourceGroupName --assignee $AadGroupId --role "Storage Blob Data Reader" --scope $id  --query roleDefinitionId
+            # az role assignment  create --resource-group $ResourceGroupName --assignee $AadGroupId --role "Reader" --scope $id  --query roleDefinitionId
+
+            az role assignment delete  --assignee $AadGroupId --role "Storage Blob Data Contributor" --scope $id  --query roleDefinitionId
+            az role assignment delete  --assignee $AadGroupId --role "Contributor" --scope $id  --query roleDefinitionId
+
+            az role assignment  create  --assignee $AadGroupId --role "Storage Blob Data Reader" --scope $id  --query roleDefinitionId
+            az role assignment  create  --assignee $AadGroupId --role "Reader" --scope $id  --query roleDefinitionId
+            
+            
+}
